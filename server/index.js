12cbc8c3904d9103a290b8def732e506829d9cee
David Osborne <david@digital-clouds.com>
2018-11-10 13:35:22 +0000

    Basic socket server set up

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send(`${__dirname}../index.html`);
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(port, () => {
+    console.log(`listening on *:${port}`);
 });
 
+io.on('connection', (socket) => {
+    console.log('user connected');
 
+    socket.on('message', (msg) => {
+        io.emit('message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+});
