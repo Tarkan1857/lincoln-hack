8cc33ca74a61012da10de6fe344736b12cadebbe
Tarkan Kemalzade <t.kemalzade@hotmail.co.uk>
2018-11-11 11:58:16 +0000

    map segments added

--- client/app.js
+++ client/app.js
@@ -7,7 +7,24 @@ const assets = [
     ['pip', './client/GFX/Hp_pip.jpg'],
     ['map', './client/GFX/Map/Map.png'],
     ['map-overlay', './client/GFX/Map/Map_Overlay.png'],
-    ['health-bar', './client/GFX/Map/hp_bank-264--189.png']
+    ['health-bar', './client/GFX/Map/hp_bank-264--189.png'],
+    ['blue0', './client/GFX/Map_Segments/Blue_01-805-217.png'],
+    ['blue1', './client/GFX/Map_Segments/Blue_02-518-548.png'],
+    ['blue2', './client/GFX/Map_Segments/Blue_03-20-0.png'],
+    ['blue3', './client/GFX/Map_Segments/Blue_04-391-234.png'],
+    ['blue4', './client/GFX/Map_Segments/Blue_05-816-475.png'],
+    ['blue5', './client/GFX/Map_Segments/Blue_06-1064-838.png'],
+    ['blue6', './client/GFX/Map_Segments/Blue_07-857-711.png'],
+    ['blue7', './client/GFX/Map_Segments/Blue_08-99-790.png'],
+    ['blue8', './client/GFX/Map_Segments/Blue_09--162--41.png'],
+    ['blue9', './client/GFX/Map_Segments/Blue_10-863--38.png'],
+    ['blue10', './client/GFX/Map_Segments/Blue_11--38-530.png'],
+    ['blue11', './client/GFX/Map_Segments/Blue_12-167--22.png'],
+    ['blue12', './client/GFX/Map_Segments/Blue_13-912-428.png'],
+    ['blue13', './client/GFX/Map_Segments/Blue_14-605-338.png'],
+    ['blue14', './client/GFX/Map_Segments/Blue_15-864-11.png'],
+    ['blue15', './client/GFX/Map_Segments/Blue_16-865-11.png'],
+    ['blue16', './client/GFX/Map_Segments/Blue_17-876-315.png'],
 ];
 
 assets.forEach((a) => PIXI.loader.add(a[0], a[1]));
--- client/game.js
+++ client/game.js
@@ -33,9 +33,42 @@ export class Game {
         game.stage.addChild(background);
         const bgOverlay = new PIXI.Sprite.fromImage("map-overlay");
         game.stage.addChild(bgOverlay);
+
+        this.spawnMapSegments(game);
         this.spawnButton(game);
 
         this.spawnHealthbar(game);
+    }
+
+    spawnMapSegments(game)
+    {
+        this.mapSegmentPos = [
+            new PIXI.Point(805, 217),
+            new PIXI.Point(518, 548),
+            new PIXI.Point(20, 0),
+            new PIXI.Point(391, 234),
+            new PIXI.Point(816, 475),
+            new PIXI.Point(1064, 838),
+            new PIXI.Point(857, 711),
+            new PIXI.Point(99, 790),
+            new PIXI.Point(162, 41),
+            new PIXI.Point(863, 38),
+            new PIXI.Point(38,530),
+            new PIXI.Point(167, 22),
+            new PIXI.Point(912, 428),
+            new PIXI.Point(605, 338),
+            new PIXI.Point(864, 11),
+            new PIXI.Point(865, 11),
+            new PIXI.Point(876, 315),
+        ];
+        this.mapSegments =[];
+        for(let seg = 0; seg < 17; ++seg)
+        {
+            this.mapSegments.push(new PIXI.Sprite.fromImage('blue'+seg));
+            this.mapSegments[this.mapSegments.length - 1].position.x = this.mapSegments[seg].x;
+            this.mapSegments[this.mapSegments.length - 1].position.y = this.mapSegments[seg].y;
+            game.stage.addChild(this.mapSegments[this.mapSegments.length -1]);
+        }
     }
 
     spawnHealthbar(game)
