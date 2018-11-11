36333dc30003ac21008ce7dce7fe1f6931750f06
David Osborne <david@digital-clouds.com>
2018-11-11 00:02:46 +0000

    Added some health pip handling stuff that is probably totally wrong

--- client/ui/health-bar.js
+++ client/ui/health-bar.js
@@ -21,8 +21,16 @@ export class HealthBar extends PIXI.Container {
     
    damage(amount) {
        // TODO: calculate how many pips remain and remove extras
+       const currentPips = this.pips.length;
        this.health -= amount;
        this.health < 0 && (this.health = 0);
 
+       // FIXME: This seems like it's probably wrong, but I am nowhere near awake enough to figure out what it should be.
+       const targetPips = this.health / this.healthPerTick;
+       const difference = currentPips - targetPips;
+       for(let i = 0; i < difference; i++) {
+           const pipToRemove = this.pips.pop();
+           pipToRemove.parent.removeChild(pipToRemove);
+       }
    }
 }
